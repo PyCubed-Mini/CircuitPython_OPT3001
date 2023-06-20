88e6d89885222a3ffacb1925ed92cb57f3c863f6
tomyyyD <thomas5d@comcast.net>
2023-06-20 09:42:04 -0400

    some comments on example

--- examples/simple.py
+++ examples/simple.py
@@ -2,8 +2,17 @@ import board
 import time
 from opt3001 import OPT3001
 
+"""
+     GND  3V3  SDA  SCL
+ADDR 0x44 0x45 0x46 0x47
+"""
+
 i2c = board.I2C()
 
+"""
+OPT3001(i2c_bus, address)
+default address is 0x44
+"""
 opt = OPT3001(i2c)
 
 while True:
